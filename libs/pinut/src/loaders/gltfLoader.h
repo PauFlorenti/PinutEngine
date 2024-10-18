8593273b8ddfb94dcf38d4afc5e23f28d95f671b
pauflorenti <pauf.97@gmail.com>
2024-10-18 18:23:02 +0200

    Changing asset gltfloader

--- libs/pinut/src/loaders/gltfLoader.h
+++ libs/pinut/src/loaders/gltfLoader.h
@@ -1,33 +1,37 @@
-//#pragma once
-//
-//namespace tinygltf
-//{
-//class Model;
-//class Node;
-//} // namespace tinygltf
-//namespace Pinut
-//{
-//class AssetManager;
-//class Device;
-//class Node;
-//class Renderable;
-//class GLTFLoader final
-//{
-//  public:
-//    GLTFLoader();
-//    void Init(Device* device, VkDescriptorSetLayout layout);
-//
-//    std::shared_ptr<Renderable> LoadFromFile(const std::filesystem::path& filepath,
-//                                             AssetManager&                assetManager);
-//
-//  private:
-//    std::shared_ptr<Node> LoadNode(const tinygltf::Model& tmodel,
-//                                   const tinygltf::Node&  tnode,
-//                                   std::shared_ptr<Node>  parent,
-//                                   AssetManager&          assetManager,
-//                                   const bool             invertNormals = false);
-//
-//    Device* m_device{nullptr};
-//    VkDescriptorSetLayout m_layout; // TODO This is temporarily hardcoded
-//};
-//} // namespace Pinut
+#pragma once
+
+namespace tinygltf
+{
+class Model;
+class Node;
+} // namespace tinygltf
+namespace RED
+{
+class Device;
+}
+namespace Pinut
+{
+class AssetManager;
+class Device;
+class Node;
+class Renderable;
+class GLTFLoader final
+{
+  public:
+    GLTFLoader();
+    void Init(std::weak_ptr<RED::Device> device, VkDescriptorSetLayout layout);
+
+    std::shared_ptr<Renderable> LoadFromFile(const std::filesystem::path& filepath,
+                                             AssetManager&                assetManager);
+
+  private:
+    std::shared_ptr<Node> LoadNode(const tinygltf::Model& tmodel,
+                                   const tinygltf::Node&  tnode,
+                                   std::shared_ptr<Node>  parent,
+                                   AssetManager&          assetManager,
+                                   const bool             invertNormals = false);
+
+    std::weak_ptr<RED::Device> m_device;
+    VkDescriptorSetLayout      m_layout; // TODO This is temporarily hardcoded
+};
+} // namespace Pinut
